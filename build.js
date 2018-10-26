7e0bc0774a6a6b6cd312a113951b237237d9dc70
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2018-10-26 12:34:02 -0300

    add files development

---
+++ build.js
@@ -0,0 +1,15 @@
+// const fs = require('fs')
+// const editorConfig = require('./src/editor.json')
+// const { classic, vivid } = require('./src/syntax')
+
+// fs.writeFileSync(
+//   './themes/OneDark-Pro.json',
+//   JSON.stringify(
+//     {
+//       ...editorConfig,
+//       ...classic
+//     },
+//     '',
+//     2
+//   )
+// )

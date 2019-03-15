a04bcd50146e3c978fc065215534241a61894db8
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2019-03-15 17:24:43 -0300

    update

--- build.js
+++ build.js
@@ -1,13 +1,37 @@
 const fs = require("fs");
 const editorConfig = require("./src/editor.json");
-let { normal } = require("./src/syntax");
+const { normal, bold } = require("./src/syntax");
 
 fs.writeFileSync(
-  "./themes/teste.json",
+  "./themes/Vulgocode-Color-Theme.json",
   JSON.stringify(
     {
       ...editorConfig,
       ...normal
+    },
+    "",
+    2
+  )
+);
+
+fs.writeFileSync(
+  "./themes/Vulgocode-Color-Theme.json",
+  JSON.stringify(
+    {
+      ...editorConfig,
+      ...bold
+    },
+    "",
+    2
+  )
+);
+
+fs.writeFileSync(
+  "./themes/OneDark-Pro-vivid.json",
+  JSON.stringify(
+    {
+      ...editorConfig,
+      ...vivid
     },
     "",
     2

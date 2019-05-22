c11b64996cbb804e98786041bfc2abcc68e8d98b
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2019-05-22 17:48:57 -0300

    📦 NEW: add github md styles, add .editorconfig, add .vscodeignore

--- build.js
+++ build.js
@@ -1,6 +1,17 @@
+"use strict";
+
 const fs = require("fs");
+var path = require("path");
+
 const editorConfig = require("./src/editor.json");
 const { normal, bold } = require("./src/syntax");
+
+const inputPath = path.join(
+  "node_modules",
+  "github-markdown-css/github-markdown.css"
+);
+
+const fullPath = path.join(__dirname, inputPath);
 
 fs.writeFileSync(
   "./themes/Vulgocode-Color-Theme.json",
@@ -25,3 +36,16 @@ fs.writeFileSync(
     2
   )
 );
+
+function updateGithubMarkdownCss(input) {
+  return (
+    `/* Generated from '${inputPath}' */\n` +
+    input.replace(/\.markdown-body/g, ".vscode-body")
+  );
+}
+
+const input = fs.readFileSync(fullPath, "utf8");
+
+fs.writeFileSync("themes/github-markdown.css", updateGithubMarkdownCss(input));
+
+fs.copyFileSync("./src/base.css", "./themes/base.css");
